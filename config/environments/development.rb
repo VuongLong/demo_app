ba6191c953509113725526d1a495e4ee18e1af95
Longvt <longvt94@gmail.com>
2015-06-18 17:51:42 +0700

    sadsad

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :letter_opener
   host = 'https://dry-caverns-9616.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
