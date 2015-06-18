33fbe1f5113deae76f54b941d53064bc1aaff880
Longvt <longvt94@gmail.com>
2015-06-18 16:58:50 +0700

    config setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
-  host = 'dry-caverns-9616.herokuapp.com'
+  host = 'https://dry-caverns-9616.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'dry-caverns-9616.herokuapp.com'
+  host = 'https://dry-caverns-9616.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
